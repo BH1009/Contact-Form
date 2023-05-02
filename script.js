9e8e9329f9a4dcca5a92517c3f92697ee0be45c8
BH1009 <hbrayan148@gmail.com>
2023-05-02 15:59:28 -0600

    Field validation

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 let errores = [];
 
-const nombre = document.document.getElementById('nombre');
-const correo = document.document.getElementById('correo');
-const asunto = document.document.getElementById('asunto');
-const opinion = document.document.getElementById('opinion');
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('correo');
+const asunto = document.getElementById('asunto');
+const opinion = document.getElementById('opinion');
 
-const enviar = document.document.getElementById('enviar');
+const enviar = document.getElementById('enviar');
 
-if(nombre === ""){
+if(nombre.value === ""){
   errores.push("El campo nombre esta vacio");
 }
 
-if(correo === ""){
+if(correo.value === ""){
   errores.push("El campo correo esta vacio");
 }
 
-if(asunto === ""){
+if(asunto.value === ""){
   errores.push("El campo asunto esta vacio");
 }
 
-if(opinion === ""){
+if(opinion.value === ""){
   errores.push("El campo opinion esta vacio");
 }
 
-enviar.addEvenListener("click", (e) => {
+enviar.addEventListener('click', (e) => {
   e.preventDefault();
-  console.log("Has enviado el formulario");
+  if(errores.length > 0 ){
+    alert(`${errores[0]} ${errores[1]} ${errores[2]} ${errores[3]}`);
+  }
+  else{
+    alert(`${nombre.value} \n ${correo.value} \n ${asunto.value} \n ${opinion.value}`);
+  }
 });
-  
