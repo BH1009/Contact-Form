94c1d550671b5bb89f814e351d24e1c47b9205b6
HB1009 <hbrayan148@gmail.com>
2023-05-04 13:29:15 -0600

    Delete PHPMailer

--- correo.php
+++ correo.php
@@ -1,9 +1,15 @@
 <?php 
 
 $destinatario = "brayan.hernandes.proglunes@gmail.com";
+
 $nombre = $_POST['nombre'];
 $correo = $_POST['correo'];
 $asunto = $_POST['asunto'];
 $opinion = $_POST['opinion'];
 
- echo json_encode($nombre . " " . $correo . " " . $asunto . " " . $opinion);
+$header = "Enviado desde HealthyPets";
+$mensaje = $opinion . "\nEscrito por: " . $nombre;  
+
+$mail($destinatario, $asunto, $mensaje, $header);
+
+echo json_encode("Su mensaje se ha enviado");
